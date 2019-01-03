6eeb5ae3a94db102be02e79b9525f28686c6a995
alex <alex_is_ok@yahoo.com>
2019-01-03 07:38:39 +0200

    random texts fix added

--- app/assets/javascripts/page3.js
+++ app/assets/javascripts/page3.js
@@ -4,10 +4,12 @@ $(document).ready(function () {
     let list = ['qwerty', 'hello', 'Hew Year', 'Jack Sparrow', 'Pikachu'];
     let min = 0;
     let max = list.length;
-    let random = Math.floor(Math.random()*(max-min+1)+min);
+
 
     $('.cool').click(function () {
+        let random = Math.floor(Math.random()*(max-min+1)+min);
         $('.p').text(list[random]);
+
     });
 
 });
